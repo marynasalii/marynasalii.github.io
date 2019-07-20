7db0e21f3af5de635893c0524c163bcf1eb9c9ad
Maryna Salii <marynasalii@gmail.com>
2019-07-20 15:50:51 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  theme: {
+        colors: {
+        'white': '#ffffff',
+        'black': '#000000',
+
+        'purple-300': '#51279B',
+        'purple-200': '#653CAD',
+        'purple-100': '#CFBCF2',
+
+        'green-200': '#2DCCA7',
+        'green-100': '#C6F7E9',
+
+        'gray-400': '#243B53',
+        'gray-300': '#334E68',
+        'gray-200': '#829AB1',
+        'gray-100': '#F0F4F8'
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '2.7rem',
+        '6xl': '3rem',
+        '7xl': '3.5rem',
+        '8xl': '4rem',
+      }
+}
+}
