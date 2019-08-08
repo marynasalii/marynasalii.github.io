ded7eb709c968314c2a9e5c6358ef344caadf28f
saliimaryna <marynasalii@gmail.com>
2019-08-08 10:48:56 -0400

    Node js dev server added

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/download', function (req, res) {
+    var file = __dirname + '/public/download.pdf';
+    res.download(file);
+});
+
+app.listen(port);
